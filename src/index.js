ff4cb75bf38da265d47cee4f3c2d7487cf61f816
Joan Llenas <joan.llenas.maso@gmail.com>
2017-05-21 14:19:03 +0200

    First commit

--- src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var distance_in_words_pipe_1 = require('./distance-in-words.pipe');
-exports.DistanceInWordsPipe = distance_in_words_pipe_1.DistanceInWordsPipe;
-var date_fns_module_1 = require('./date-fns.module');
-exports.DateFnsModule = date_fns_module_1.DateFnsModule;
-//# sourceMappingURL=index.js.map
