099cc4b8e72fd38274904ced947e9ffe95393dc0
Joan Llenas <joanllenas@users.noreply.github.com>
2020-07-26 18:50:04 +0200

    Revert "Pure version of impure pipes (#338)"
    This reverts commit 322e7988c82bba335ed51844a6936649445377ea.

--- projects/ngx-date-fns/src/lib/format-distance.pipe.spec.ts
+++ projects/ngx-date-fns/src/lib/format-distance.pipe.spec.ts
@@ -2,7 +2,6 @@ import { ChangeDetectorRef } from '@angular/core';
 import { DateFnsConfigurationService } from './date-fns-configuration.service';
 import { FormatDistancePipe } from './format-distance.pipe';
 import { es } from 'date-fns/locale';
-import { FormatDistancePurePipe } from './format-distance.pure.pipe';
 
 describe('FormatDistancePipe', () => {
   let pipe: FormatDistancePipe;
@@ -61,51 +60,5 @@ describe('FormatDistancePipe', () => {
         locale: es
       })
     ).toBe('más de 1 año');
-  });
-});
-
-describe('FormatDistancePipe Pure', () => {
-  const createPipe = () =>
-    new FormatDistancePurePipe(new DateFnsConfigurationService());
-
-  it('should display the distance between 2 July 2014 and 1 January 2015', () => {
-    expect(
-      createPipe().transform(new Date(2014, 6, 2), new Date(2015, 0, 1))
-    ).toBe('6 months');
-  });
-
-  it('should display the distance between 1 January 2015 00:00:15 and 1 January 2015 00:00:00, including seconds', () => {
-    expect(
-      createPipe().transform(
-        new Date(2015, 0, 1, 0, 0, 15),
-        new Date(2015, 0, 1, 0, 0, 0),
-        { includeSeconds: true }
-      )
-    ).toBe('less than 20 seconds');
-  });
-
-  it('should display the distance from 1 January 2016 to 1 January 2015, with a suffix', () => {
-    expect(
-      createPipe().transform(new Date(2015, 0, 1), new Date(2016, 0, 1), {
-        addSuffix: true
-      })
-    ).toBe('about 1 year ago');
-  });
-
-  it('should display the distance between 1 August 2016 and 1 January 2015 in Spanish', () => {
-    expect(
-      createPipe().transform(new Date(2016, 7, 1), new Date(2015, 0, 1), {
-        locale: es
-      })
-    ).toBe('más de 1 año');
-  });
-
-  it('should display the distance between 1 August 2016 and 1 January 2015 in Spanish when locale provided via config', () => {
-    const conf = new DateFnsConfigurationService();
-    conf.setLocale(es);
-    const pipe = new FormatDistancePurePipe(conf);
-    expect(pipe.transform(new Date(2016, 7, 1), new Date(2015, 0, 1))).toBe(
-      'más de 1 año'
-    );
   });
 });
