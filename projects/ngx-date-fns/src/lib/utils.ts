7fff968ac7635a71f371953bc9b65256ea85125c
Joan Llenas <joanllenas@users.noreply.github.com>
2025-01-19 15:12:37 +0100

    fix(utils): Allow the use of strings as input for dates (#411)
    Fixes #409

--- projects/ngx-date-fns/src/lib/utils.ts
+++ projects/ngx-date-fns/src/lib/utils.ts
@@ -3,4 +3,12 @@ import { DateFnsInputDate } from './types';
 
 export const isValidDate = (
   date: DateFnsInputDate | null | undefined
-): date is Date => !(date === undefined || date === null || !isValid(date));
+): date is Date => {
+  if (date === undefined || date === null) {
+    return false;
+  }
+  if (typeof date === 'string' && date.length > 0) {
+    return true;
+  }
+  return isValid(date);
+};
